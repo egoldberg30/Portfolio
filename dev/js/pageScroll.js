cf226a62a2d15072aac98d579262f01bde79fe9d
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-03-08 11:51:43 -0500

    renaming pages

--- dev/js/pageScroll.js
+++ dev/js/pageScroll.js
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollToPlugin);
 
-let idArray = ["#graysection","#redsection","#bluesection", "#violetsection", "#yellowsection"];
+let idArray = ["#portfolio","#resume","#aboutme"];
 
 export function scrollPage(index){
     gsap.to(window, {duration: 2, scrollTo:idArray[index]});
